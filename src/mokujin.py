d66ae41feacdb4ce645178346ffb723078ae273a
dartbored <43121194+dartbored@users.noreply.github.com>
2021-03-05 11:11:27 -0600

    added startup

--- src/mokujin.py
+++ src/mokujin.py
@@ -11,6 +11,9 @@ from discord.ext import commands
 from src import tkfinder, util
 from src.resources import embed, const
 from github import Github
+from resources.tokeny import startup
+
+startup()
 
 base_path = os.path.dirname(__file__)
 config = configurator.Configurator(os.path.abspath(os.path.join(base_path, "resources", "config.json")))
