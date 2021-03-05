8e6322884a30485e4f12cbac1b6daa0f8610cf04
dartbored <43121194+dartbored@users.noreply.github.com>
2021-03-05 10:51:30 -0600

    added dumps

--- src/resources/tokeny.py
+++ src/resources/tokeny.py
@@ -4,11 +4,11 @@ import json
 
 TOKENN = os.environ['TOKEN']
 
-json_object = {
+json_object = json.dumps({
   "DISCORD_TOKEN": TOKENN,
   "GITHUB_TOKEN": "",
   "FEEDBACK_CHANNEL_ID" : 0
-}
+})
 
 with open("config.json", "w") as outfile: 
     outfile.write(json_object) 
