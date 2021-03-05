619d827d60d8de881def991497757b9e2f5af839
dartbored <43121194+dartbored@users.noreply.github.com>
2021-03-05 10:50:11 -0600

    fixing token issue

---
+++ src/resources/tokeny.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+
+TOKENN = os.environ['TOKEN']
+
+json_object = {
+  "DISCORD_TOKEN": TOKENN,
+  "GITHUB_TOKEN": "",
+  "FEEDBACK_CHANNEL_ID" : 0
+}
+
+with open("config.json", "w") as outfile: 
+    outfile.write(json_object) 
