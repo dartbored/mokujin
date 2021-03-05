f04e1c38368e90554225143f55b53c39bd642a14
dartbored <43121194+dartbored@users.noreply.github.com>
2021-03-05 12:04:09 -0600

    fixing indents

--- src/resources/tokeny.py
+++ src/resources/tokeny.py
@@ -5,13 +5,15 @@ def startup():
     TOKENN = os.environ['TOKEN']
     GHT = os.environ['GH']
     
-    print(f'TOKEN IS READY: {TOKENN[:7]}')
-
-    json_object = json.dumps({
+    print(f'TOKEN IS READY: {TOKENN[:4]}')
+	
+	datao = {
 		"DISCORD_TOKEN": TOKENN,
 		"GITHUB_TOKEN": GHT,
 		"FEEDBACK_CHANNEL_ID" : 0
-    })
+    }
+
+    json_object = json.dumps(datao)
 
     with open("config.json", "w") as outfile: 
-      outfile.write(json_object)
+    	outfile.write(json_object)
